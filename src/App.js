83a95763fecd6a879c2774ab61b4d06bb08e7d39
Listopad02 <123ayetvbass@gmail.com>
2021-11-05 20:06:19 +0300

    new components and styles added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Layout from './hoc/Layout/Layout';
+import Quiz from './containers/Quiz/Quiz';
 
 class App extends React.Component {
   render() {
     return (
       <Layout>
-        <div style={{width: 400, border: '1px solid #ccc'}}>
-          <h1>Layout</h1>
-        </div>
+        <Quiz />
       </Layout>
     );
   }
