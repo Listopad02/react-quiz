55b4bb9903f09a9be4f0dfe3cd5c145d98f4464a
Listopad02 <123ayetvbass@gmail.com>
2021-11-07 20:35:05 +0300

    question answering realized

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -4,15 +4,28 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz';
 
 class Quiz extends React.Component {
     state = {
+        activeQuestion: 0,
         quiz: [
             {
                 question: 'How are you?',
                 rightAnswerId: 1,
+                id: 1,
                 answers: [
                     {text: 'Good', id: 1},
                     {text: 'Not bad', id: 2},
                     {text: 'Just bad', id: 3},
                     {text: 'Awful', id: 4}
+                ]
+            },
+            {
+                question: 'Who is Tom Cruz',
+                rightAnswerId: 2,
+                id: 2,
+                answers: [
+                    {text: 'Singer', id: 1},
+                    {text: 'Actor', id: 2},
+                    {text: 'Writer', id: 3},
+                    {text: 'Nobody', id: 4}
                 ]
             }
         ]
@@ -29,7 +42,9 @@ class Quiz extends React.Component {
                     <h1>Answer all the questions</h1>
                     <ActiveQuiz answers={this.state.quiz[0].answers}
                                 question={this.state.quiz[0].question}
-                                onAnswerClick={this.onAnswerClickHandler} />
+                                onAnswerClick={this.onAnswerClickHandler}
+                                quizLength={this.state.quiz.length}
+                                answerNumber={this.state.activeQuestion + 1} />
                 </div>
             </div>
         )
