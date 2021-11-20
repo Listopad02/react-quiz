ba3f0275bdde154e4d13054487f8ea48c9342756
Listopad02 <123ayetvbass@gmail.com>
2021-11-20 22:21:00 +0300

    new page for authorization and registration added

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,32 @@
 import React from "react";
+import classes from './Auth.css';
+import Button from '../../components/UI/Button/Button'
 
 export default class Auth extends React.Component {
+    loginHandler = () => {
+        console.log('login')
+    }
+
+    registerHandler = () => {
+        console.log('register')
+    }
+
+    submitHandler = e => {
+        e.preventDefault();
+    }
+
     render () {
         return (
-            <div>
-                <h1>Auth</h1>
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Authorization</h1>
+                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+                        <input type="text" />
+                        <input type="text" />
+                        <Button type="success" onClick={this.loginHandler}>Enter</Button>
+                        <Button type="primary" onClick={this.registerHandler}>Register</Button>
+                    </form>
+                </div>
             </div>
         )
     }
