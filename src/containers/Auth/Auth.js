7410e5f6923b4848717824ca4da1d4465d479e6a
Listopad02 <123ayetvbass@gmail.com>
2021-11-21 21:36:30 +0300

    email validation fully realized + minlength validation for password

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React from "react";
 import classes from './Auth.css';
 import Button from '../../components/UI/Button/Button';
 import Input from "../../components/UI/Input/Input";
+import is from 'is_js';
 
 export default class Auth extends React.Component {
     state = {
@@ -45,8 +46,37 @@ export default class Auth extends React.Component {
         e.preventDefault();
     }
 
+    validateControl(value, validation) {
+        if (!validation) {
+            return true;
+        }
+        let isValid = true;
+        if (validation.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+        if (validation.email) {
+            isValid = is.email(value) && isValid;
+        }
+        if (validation.minLength) {
+            isValid = value.length >= validation.minLength && isValid;
+        }
+
+        return isValid;
+    }
+
     onChangeHandler = (e, controlName) => {
-        console.log('test');
+        const formControls = { ...this.state.formControls };
+        const control = { ...formControls[controlName] };
+
+        control.value = e.target.value;
+        control.touched = true;
+        control.valid = this.validateControl(control.value, control.validation)
+
+        formControls[controlName] = control;
+
+        this.setState({
+            formControls
+        })
     }
 
     renderInputs = () => {
