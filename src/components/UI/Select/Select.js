e557fb4fbd54e4c128cb409b5970b4bc445f5a56
Listopad02 <123ayetvbass@gmail.com>
2021-11-27 23:39:47 +0300

    unimportant changes

--- src/components/UI/Select/Select.js
+++ src/components/UI/Select/Select.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from './Select.css';
 
 const Select = props => {
-    const htmlFor = `${props.label}=${Math.random()}`
+    const htmlFor = `${props.label}-${Math.random()}`;
 
     return (
         <div className={classes.Select}>
